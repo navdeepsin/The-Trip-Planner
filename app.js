75f210ea8cee53fe374040ec58a93329e02eb395
Navdeepsin <navdeepsin31@gmail.com>
2020-05-23 13:58:49 -0500

    adds event listener on destination form

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const formClassEle = document.querySelector('.origin-form');
+const originFormEle = document.querySelector('.origin-form');
 const originsEle = document.querySelector('.origins');
-
-
-
-
+const destinationFormEle = document.querySelector('.destination-form');
+const destinationsEle = document.querySelector('.destinations');
 const accessToken = 'pk.eyJ1IjoibmF2ZGVlcHNpbjMxIiwiYSI6ImNrYTZ1NDJvbTBjcjcyeW11enZzZDV2aHEifQ.isANPuVW0B-Xg7MYjfZPnw';
 
-formClassEle.onsubmit = e => {
+originFormEle.onsubmit = e => {
   const input = e.target.querySelector('input');
 
   if (input.value.length > 0) {
-    displayLocations(input.value);
+    displayStartingLocations(input.value);
   }
 
   input.value = "";
   e.preventDefault();
 }
 
-function displayLocations(query) {
+function displayStartingLocations(query) {
   fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${accessToken}&limit=10&bbox=-97.325875, 49.766204, -96.953987, 49.99275`)
     .then(resp => resp.json())
     .then (data => {
@@ -54,6 +52,53 @@ originsEle.onclick = e => {
   clicked.classList.add("selected");
 } 
 
+destinationFormEle.onsubmit = e => {
+  const input = e.target.querySelector('input');
+
+  if (input.value.length > 0) {
+    displayDestinationLocations(input.value);
+  }
+
+  input.value = "";
+  e.preventDefault();
+}
+
+// function displayDestinationLocations(query) {
+//   fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${accessToken}&limit=10&bbox=-97.325875, 49.766204, -96.953987, 49.99275`)
+//     .then(resp => resp.json())
+//     .then (data => {
+//       destinationsEle.innerHTML = "";
+//       locationsHTML = "";
+
+//       data.features.forEach(location => {
+//         if(location.properties.address !== undefined) {
+//           locationsHTML += `
+//             <li data-long="${location.geometry.coordinates[0]}" data-lat="${location.geometry.coordinates[1]}" class="">
+//               <div class="name">${location.text}</div>
+//               <div>${location.properties.address}</div>
+//             </li>`
+//         }else {
+//           locationsHTML += `
+//             <li data-long="${location.geometry.coordinates[0]}" data-lat="${location.geometry.coordinates[1]}" class="">
+//               <div class="name">${location.text}</div>
+//               <div>Winnipeg</div>
+//             </li>`
+//         }
+//         destinationsEle.innerHTML = locationsHTML;
+//       });
+//     })
+// }
+
+// destinationsEle.onclick = e => {
+//   const listOfLi = document.querySelectorAll('.destinations li');
+//   listOfLi.forEach(li => {
+//     if(li.classList.value === "selected") {
+//       li.classList.remove("selected");
+//     }
+//   })
+//   const clicked = e.target.closest('li');
+//   clicked.classList.add("selected");
+// } 
  
 
 
