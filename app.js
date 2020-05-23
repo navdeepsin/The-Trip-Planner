7e0b2b12b49971235b3f291dcb0d34abf37dd877
Navdeepsin <navdeepsin31@gmail.com>
2020-05-23 00:01:02 -0500

    Prints the results of starting location input

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const formClassEle = document.querySelector('.origin-form');
-console.log(formClassEle);
+const originsEle = document.querySelector('.origins');
+
 
 const accessToken = 'pk.eyJ1IjoibmF2ZGVlcHNpbjMxIiwiYSI6ImNrYTZ1NDJvbTBjcjcyeW11enZzZDV2aHEifQ.isANPuVW0B-Xg7MYjfZPnw';
 
@@ -18,6 +19,24 @@ function displayLocations(query) {
   fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${accessToken}&limit=10&bbox=-97.325875, 49.766204, -96.953987, 49.99275`)
     .then(resp => resp.json())
     .then (data => {
-      console.log(data);
+      originsEle.innerHTML = "";
+      locationsHTML = "";
+
+      data.features.forEach(location => {
+        if(location.properties.address !== undefined) {
+          locationsHTML += `
+            <li data-long="${location.geometry.coordinates[0]}" data-lat="${location.geometry.coordinates[1]}" class="">
+              <div class="name">${location.text}</div>
+              <div>${location.properties.address}</div>
+            </li>`
+        }else {
+          locationsHTML += `
+            <li data-long="${location.geometry.coordinates[0]}" data-lat="${location.geometry.coordinates[1]}" class="">
+              <div class="name">${location.text}</div>
+              <div>Winnipeg</div>
+            </li>`
+        }
+      });
+      originsEle.innerHTML = locationsHTML;
     })
 }
